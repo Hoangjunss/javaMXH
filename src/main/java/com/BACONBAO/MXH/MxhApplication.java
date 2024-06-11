d56e68e5e6c36349760262cd40f4f78be6d35802
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-06-11 19:34:36 +0700

    add

---
+++ src/main/java/com/BACONBAO/MXH/MxhApplication.java
@@ -0,0 +1,13 @@
+package com.BACONBAO.MXH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BACONBAO/MXH/MxhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BACONBAO.MXH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
